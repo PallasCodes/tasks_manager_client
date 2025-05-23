e4854e489bc4295560d90b98e290735a82c52cde
pallascodes <pallascodes@gmail.com>
2025-05-23 11:31:00 -0600

    feat: refine Sidebar layout and improve Accordion component styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,11 +11,14 @@ import { Checkbox } from './ui/checkbox'
 const Sidebar = () => {
   return (
     <aside className={`max-w-72 w-full min-h-[calc(screen - 50px)] p-4`}>
-      <Button variant="secondary" className="text-md w-full rounded-lg">
+      <Button
+        variant="secondary"
+        className="text-md rounded-lg shadow-none py-6! px-4!"
+      >
         <Plus className="size-5" /> Create
       </Button>
-      <div className="flex flex-col mt-8">
-        <Button className="bg-blue-100 rounded-3xl text-gray-900 justify-start gap-3 px-4! shadow-none! hover:bg-blue-200 transition-colors">
+      <div className="flex flex-col mt-7">
+        <Button className="bg-blue-50 rounded-3xl text-gray-900 justify-start gap-3 px-4! shadow-none! hover:bg-blue-200 transition-colors">
           <CircleCheckBig />
           All Tasks
         </Button>
@@ -53,7 +56,7 @@ const Sidebar = () => {
 
       <Button
         variant="ghost"
-        className="justify-start gap-3 rounded-3xl px-4! w-full font-semibold"
+        className="justify-start gap-3 rounded-3xl px-4! w-full font-semibold mt-4"
       >
         <Plus className="size-5" />
         Create List
