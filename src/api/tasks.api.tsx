3578b431663b192cd1bc02643c2c1eb9833e6917
pallascodes <pallascodes@gmail.com>
2025-05-23 16:29:21 -0600

    feat: implement task creation functionality in ListCard component and enhance styling

---
+++ src/api/tasks.api.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+import { toast } from 'sonner'
+
+import { api } from '.'
+
+const MODULE_PREFIX = '/tasks'
+
+type createTaskPayload = {
+  title: string
+  listId: string
+}
+
+export const useCreateTask = () => {
+  const queryClient = useQueryClient()
+
+  const createTaskRequest = async (
+    payload: createTaskPayload
+  ): Promise<createTaskPayload> => {
+    try {
+      const response = await api.post(MODULE_PREFIX, payload)
+      return response.data
+    } catch (error: any) {
+      throw new Error(error.response?.data?.message || 'Could not create task')
+    }
+  }
+
+  const {
+    mutateAsync: createTask,
+    isLoading,
+    error,
+    reset
+  } = useMutation({
+    mutationFn: createTaskRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['fetchLists'])
+    }
+  })
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.toString())
+      reset()
+    }
+  }, [error, reset])
+
+  return {
+    createTask,
+    isLoading
+  }
+}
--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -1,28 +1,73 @@
-import type { List as ListCard } from '@/types/list.interface'
 import { CheckLine, Circle, EllipsisVertical } from 'lucide-react'
+import { useState, type FormEvent } from 'react'
+
+import { useCreateTask } from '@/api/tasks.api'
+import type { List as ListCard } from '@/types/list.interface'
 import { Button } from './ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import { Input } from './ui/input'
 
 const ListCard = ({ list }: { list: ListCard }) => {
+  const [showNewTaskInput, setShowNewTaskInput] = useState(false)
+  const [newTask, setNewTask] = useState('')
+  const { createTask, isLoading } = useCreateTask()
+
+  const handleAddTask = async (e?: FormEvent<HTMLFormElement>) => {
+    e?.preventDefault()
+    setShowNewTaskInput(false)
+
+    const trimmedNewTask = newTask.trim()
+    if (trimmedNewTask === '') return
+
+    try {
+      await createTask({
+        listId: list.id as string,
+        title: trimmedNewTask
+      })
+      setNewTask('')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <Card className="shrink-0 grow min-w-sm max-w-lg w-full gap-2! py-5!">
-      <CardHeader className="relative px-4!">
-        <CardTitle>{list.title}</CardTitle>
+    <Card className="shrink-0 grow min-w-sm max-w-lg w-full gap-0! py-5! overflow-y-scroll list-card">
+      <CardHeader className="px-0! top-0 sticky bg-white">
+        <CardTitle className="px-4">{list.title}</CardTitle>
         <Button
           variant="ghost"
           className="rounded-full absolute top-[-14px] right-1 size-8"
         >
           <EllipsisVertical className="size-5 text-gray-700" />
         </Button>
-      </CardHeader>
-      <CardContent className="flex flex-col px-0!">
         <Button
           variant="ghost"
           className="text-blue-400 justify-start px-4! hover:bg-blue-50 transition-colors hover:text-text-blue-600"
+          onClick={() => setShowNewTaskInput(true)}
         >
           <CheckLine />
           Add a task
         </Button>
+
+        {showNewTaskInput && (
+          <div className="w-full flex items-center px-2 py-1">
+            <Button variant="ghost" className="rounded-full size-7">
+              <Circle />
+            </Button>
+            <form onSubmit={(e) => handleAddTask(e)}>
+              <Input
+                placeholder="Title"
+                className="border-none shadow-none outline-none focus:outline-none! focus:ring-0! focus:border-transparent! px-[6px]!"
+                autoFocus
+                onBlur={() => handleAddTask()}
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+              />
+            </form>
+          </div>
+        )}
+      </CardHeader>
+      <CardContent className="flex flex-col px-0!">
         {list.tasks?.map((task) => (
           <div
             key={task.id}
